a29969ca2ed2edc8dd38db5d647827b013018acb
Darya Kauzova <amayasuzuki12@gmail.com>
2025-04-18 12:42:59 +0300

    deleted unneccesery comments

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import { Provider } from 'react-redux';
-// import * as React from 'react';
 import './App.css';
 import SwitchThem from './components/SwitchThem';
 import store from './components/store';
-// import { useEffect } from 'react';
 
 function App() {
 
-  // console.log(localStorage.getItem('theme'));
-  
-  // const initialState = localStorage.getItem('theme') || 'темную';
   return (
-    
+
     <div id='infinity' data-theme='light-theme' className="main__container ">
       <Provider store={store}>
         <SwitchThem />
@@ -20,7 +15,4 @@ function App() {
   );
 }
 
-// // localStorage.clear()
 export default App;
-
-// 
